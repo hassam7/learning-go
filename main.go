4c66fe34dd6d5da0f44b0427c9c1d6ed44abc378
Hassam7 <hassam.ali7@hotmail.com>
2018-02-18 16:58:08 +0500

    remove extra code

--- main.go
+++ main.go
@@ -5,9 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to Go")
 	cards := newDeck()
-	hand, remaining := deal(cards, 5)
-	hand.print()
-	fmt.Println("============================")
-	remaining.print()
-
+	fmt.Println(cards.toString())
 }
